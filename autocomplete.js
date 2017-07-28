c623127a12483288b5227c5afac200486e9c8706
Grzegorz Futa <grzesiekfuta@gmail.com>
2017-07-28 15:59:26 +0200

    Add files via upload

---
+++ autocomplete.js
@@ -0,0 +1,40 @@
+$(function() {
+    
+ $('.autocomplete').autocomplete({
+
+    data: {
+
+        "bekon": "./img/bcn.png",
+
+        "bułki": "./img/bulki.jpg",
+
+        "chipsy": "./img/chipsy.jpg",
+
+        "chleb": "./img/chleb.png",
+
+        "jaja": "./img/jaja.jpg",
+
+        "kawa": "./img/kawa.jpg",
+
+        "ketchup": "./img/ketchup.jpg",
+
+        "kiełbasa": "./img/kielbasa.jpg",
+
+        "masło": "./img/maslo.jpg",
+
+        "mleko": "./img/mleko.jpg",
+
+        "musztarda": "./img/musztarda.jpeg",
+
+        "piwo": "./img/piwo.png",
+
+        "płatki": "./img/platki.jpg",
+
+        "sos czosnkowy": "./img/sos czosnkowy.jpg"
+
+    }
+
+});
+});
+
+
